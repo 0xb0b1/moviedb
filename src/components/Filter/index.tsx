8619d3a4ddee7a71318f1d3bcc87f4972c0ccd89
Paulo Vicente <vcente82@gmail.com>
2022-02-01 00:07:31 -0300

    skeleton filter component

---
+++ src/components/Filter/index.tsx
@@ -0,0 +1,98 @@
+import styles from "./styles.module.scss";
+
+const genres = [
+  {
+    id: 28,
+    name: "Action",
+  },
+  {
+    id: 12,
+    name: "Adventure",
+  },
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 35,
+    name: "Comedy",
+  },
+  {
+    id: 80,
+    name: "Crime",
+  },
+  {
+    id: 99,
+    name: "Documentary",
+  },
+  {
+    id: 18,
+    name: "Drama",
+  },
+  {
+    id: 10751,
+    name: "Family",
+  },
+  {
+    id: 14,
+    name: "Fantasy",
+  },
+  {
+    id: 36,
+    name: "History",
+  },
+  {
+    id: 27,
+    name: "Horror",
+  },
+  {
+    id: 10402,
+    name: "Music",
+  },
+  {
+    id: 9648,
+    name: "Mystery",
+  },
+  {
+    id: 10749,
+    name: "Romance",
+  },
+  {
+    id: 878,
+    name: "Science Fiction",
+  },
+  {
+    id: 10770,
+    name: "TV Movie",
+  },
+  {
+    id: 53,
+    name: "Thriller",
+  },
+  {
+    id: 10752,
+    name: "War",
+  },
+  {
+    id: 37,
+    name: "Western",
+  },
+];
+
+export const Filter = () => {
+  return (
+    <div className={styles.container}>
+      <section>
+        <h1>Milhões de files, séries e pessoas para descobrir. Explore já.</h1>
+      </section>
+      <div className={styles.filter}>
+        <h4>FILTRE POR:</h4>
+        <div>
+          {genres.map((item) => (
+            <button key={item.id}>{item.name}</button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
