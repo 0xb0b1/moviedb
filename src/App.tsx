7d4d28eedc00306ea12a5085df3702f5ad40afa2
Paulo Vicente <vcente82@gmail.com>
2022-02-03 13:39:20 -0300

    movie id context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
-import { Filter } from "./components/Filter";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 import { Header } from "./components/Header";
-
-import styles from "./styles/home.module.scss";
+import { MovieIdProvider } from "./hooks/useMovieId";
+import { Home } from "./pages/Home";
+import { MovieDetails } from "./pages/MovieDetails";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className={styles.homeContainer}>
+    <MovieIdProvider>
       <Header />
-      <Filter />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie-details" element={<MovieDetails />} />
+      </Routes>
+    </MovieIdProvider>
   );
 }
 
