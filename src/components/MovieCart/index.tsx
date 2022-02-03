a587ccc05ac600a37d2f6d47f4eba33c74e2f6e7
Paulo Vicente <vcente82@gmail.com>
2022-02-03 13:37:25 -0300

    context to handle unique movie id (may not be the best solution)

--- src/components/MovieCart/index.tsx
+++ src/components/MovieCart/index.tsx
@@ -1,21 +1,29 @@
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useMovieId } from "../../hooks/useMovieId";
+
 import styles from "./styles.module.scss";
 
 interface IProps {
   poster_path: string;
   title: string;
   release_date: string;
-  id?: number;
+  id: number;
 }
 
 export const MovieCart = ({ poster_path, title, release_date, id }: IProps) => {
+  const { handleMovieId } = useMovieId();
+
   return (
     <div className={styles.containerBox}>
-      <div className={styles.contentBox}>
-        <img src={poster_path} alt="poster" />
+      <Link to="movie-details" onClick={() => handleMovieId(id)}>
+        <div className={styles.contentBox}>
+          <img src={poster_path} alt="poster" />
 
-        <span>{title}</span>
-        <span>{release_date}</span>
-      </div>
+          <span>{title}</span>
+          <span>{release_date}</span>
+        </div>
+      </Link>
     </div>
   );
 };
